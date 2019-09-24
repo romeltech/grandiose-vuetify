b2f16fe35da41acc6d1f9483001c06cc9326ed00
mel <indemnedev@gmail.com>
2019-09-24 21:17:08 +0400

    fresh install of laravel 6 and vuetify

---
+++ resources/js/components/HeaderNavigation.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <v-app-bar
+        color="deep-purple accent-4"
+        dark
+        >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Page title</v-toolbar-title>
+
+        <div class="flex-grow-1"></div>
+
+        <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                        <div class="d-flex justify-center">
+                                <v-color-picker></v-color-picker>
+                              </div>
+                              
+                        <div class="text-center">
+                            <v-menu offset-y>
+                                <template v-slot:activator="{ on }">
+                                <v-btn
+                                    color="primary"
+                                    dark
+                                    v-on="on"
+                                >
+                                    Dropdown
+                                </v-btn>
+                                </template>
+                                <v-list>
+                                <v-list-item>
+                                    <v-list-item-title>sadsa asd as d</v-list-item-title>
+                                </v-list-item>
+                                </v-list>
+                            </v-menu>
+                        </div>
+
+                    
+                    {{-- <example-component></example-component> --}}
+                    {{-- <header-navigation></header-navigation> --}}
+                    <custom-button></custom-button>
+                    <div class="text-center">
+                        <v-btn>Button</v-btn>
+                    </div>
+
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
